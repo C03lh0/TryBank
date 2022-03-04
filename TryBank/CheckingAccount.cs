7dca2b6a346b97592d79079f3f411fdd6a0e229b
Matheus Coelho Muniz dos Santos <macomusa14@gmail.com>
2022-03-04 18:19:49 -0300

    Adicionar arquivos de projeto.

---
+++ TryBank/CheckingAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryBank
+{
+    public class CheckingAccount
+    {
+        public string holder;
+        public int agency;
+        public int numberAccount;
+        public double balance = 100;
+
+        public bool Withdraw(double value)
+        {
+            if (this.balance < value)
+            {
+                return false;
+            }
+
+            this.balance -= value;
+            return true;
+        }
+
+        public void Deposit(double value)
+        {
+            this.balance += value;
+        }
+
+        public bool Transfer(double value, CheckingAccount destinationAccount)
+        {
+            if (this.balance < value)
+            {
+                return false;
+            }
+
+            this.balance -= value;
+            destinationAccount.Deposit(value);
+            return true;
+        }
+    }
+}
